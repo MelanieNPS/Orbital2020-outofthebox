ced0053e01bd6ac1dad362884bcaed8c3d1eb549
Chow Boon Wei <chowboonwei@gmail.com>
2020-06-12 14:42:52 +0800

    changed port

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const app = require("express")();
 const {Pool} = require('pg')
 require("dotenv").config()
-var connectionString= "postgres://fivlromeqlnjrg:edd0b21807802ec53e54e005a67ffc8f01d5f8efc80db81a890614b0a77ee3b7@ec2-54-246-87-132.eu-west-1.compute.amazonaws.com:5432/dasj74f4uq4dcp"
 
 const pool = new Pool({
-  connectionString: connectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   },
@@ -20,4 +19,4 @@ app.get("/all", async (req,res) => {
 
 })
 
-app.listen( 9000, () => console.log("Listening on port 9000"))
+app.listen( 9000 || process.env.PORT, () => console.log("Listening on port 9000"))
