c8a2711c36fc25e2407f0a3d6b1cbd3c4144b4fa
melanie <melanienps@gmail.com>
2020-06-12 13:33:35 +0800

    Set connection string

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { Client } = require('pg')
 require("dotenv").config()
+var connectionString= "postgres://fivlromeqlnjrg:edd0b21807802ec53e54e005a67ffc8f01d5f8efc80db81a890614b0a77ee3b7@ec2-54-246-87-132.eu-west-1.compute.amazonaws.com:5432/dasj74f4uq4dcp"
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: connectionString,
   ssl: {
     rejectUnauthorized: false
   }
